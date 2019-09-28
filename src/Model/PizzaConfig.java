318eb29b9e7eb89c95227d9087f3d179ec8f9857
patrick ntwari <patrick_ntwari@live.com>
2019-09-28 18:27:38 +0200

    PNT:Fully created the PizzaConfig class

--- src/Model/PizzaConfig.java
+++ src/Model/PizzaConfig.java
@@ -5,10 +5,92 @@
  */
 package Model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Ntwali
  */
-public class PizzaConfig {
+public class PizzaConfig implements Serializable{
     
+    
+    private double baseprice;   
+    private String size;
+    
+    private double delivery;
+    private String name;
+    private OptionSet[] optionsets; 
+    
+    
+    public PizzaConfig(){
+        
+    }
+
+    public PizzaConfig(double baseprice,String size,double delivery,String name,OptionSet[] optionsets){
+        
+        this.baseprice = baseprice;
+        this.size   = size;
+        this.delivery = delivery;
+        this.name     = name;
+        this.optionsets = optionsets;
+ 
+    }
+
+    public double getBaseprice() {
+        return baseprice;
+    }
+
+    public void setBaseprice(double baseprice) {
+        this.baseprice = baseprice;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(double delivery) {
+        this.delivery = delivery;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+   public OptionSet[] getOptionsets() {
+        return optionsets;
+    }
+
+    public void setOptionsets(String name,String[] option,double totalPrice) {
+        this.optionsets = optionsets;
+    }
+    
+    public void find(String name ){
+        
+    }
+    
+    public void delete(){
+        
+    }
+    
+    public void update(){
+        
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+ 
 }
