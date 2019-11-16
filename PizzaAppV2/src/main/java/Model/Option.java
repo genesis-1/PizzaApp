f47dbb15c48957b900dc13d9c61224f2d31a4d41
root <root@loic>
2019-11-16 16:43:37 +0200

    added Hibernateconfig

---
+++ PizzaAppV2/src/main/java/Model/Option.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author root
+ */
+public class Option {
+
+        private String name;
+        private double price;
+
+        public Option() {
+        }
+
+        public Option(String name) {
+            this.name = name;
+        }
+
+        public Option(String name, double price) {
+
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+//         public void findOPtion(String name){
+//        
+//    }
+}
