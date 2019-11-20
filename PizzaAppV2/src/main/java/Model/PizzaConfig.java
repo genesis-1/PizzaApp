051b59d0c47f1bdd968d5faa6e7b8e491904475b
patrick ntwari <patrick_ntwari@live.com>
2019-11-20 17:40:28 +0200

    added space

--- PizzaAppV2/src/main/java/Model/PizzaConfig.java
+++ PizzaAppV2/src/main/java/Model/PizzaConfig.java
@@ -15,10 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-/**
- *
- * @author Ntwali
- */
+
 @Entity
 public class PizzaConfig implements Serializable{
 
